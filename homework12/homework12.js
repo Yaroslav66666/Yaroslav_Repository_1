e1fca296a282fb302aa352e32e190c9918596eca
Yaroslav66666 <aroslaveremenko6@gmail.com>
2025-06-28 12:03:50 +0300

    homework12

---
+++ homework12/homework12.js
@@ -0,0 +1,44 @@
+console.log("Завдання 1")         // Завдання 1
+                            
+let x = 11;                     
+let y = 10;
+alert(x, y);
+
+let bool_res = x > y;
+
+console.log(bool_res);
+
+
+console.log("Завдання 2")              // Завдання 2
+
+let a = 20;
+let b = 20;
+alert(x, y); 
+
+let bool_res2 = a == b;
+
+console.log(bool_res2);
+
+
+console.log("Завдання 3")             // Завдання 3
+
+let q = 31;
+let w = 30;
+alert(q, w);
+
+let bool_res3 = q != w;
+
+console.log(bool_res3)
+
+
+console.log("Завдання 4")       // Завдання 4
+
+let age = 190;
+let votable = (age, 180) ? "Ваш зріст 180см або більше" : "Занадто високий";
+alert(votable);
+
+if ( 190>180 ) {
+   console.log("Занадто високий")
+}  else {
+    console.log("Ваш зріст 180см або більше")
+}
