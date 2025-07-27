fb7db48309d598d963cca5f0fca396b1eaa32281
Yaroslav66666 <aroslaveremenko6@gmail.com>
2025-07-27 10:44:09 +0300

    homework18

--- homework18/homework18.js
+++ homework18/homework18.js
@@ -1,45 +1,29 @@
-document.cookie = "user=Sasha";
-document.cookie = "age=25";
-document.cookie = "Cookie Saving Day=03.06.2025";
+document.getElementById("cookieForm").addEventListener("submit", function(e) {
+      e.preventDefault();
 
+      let user = document.getElementById("userInput").value;
+      let age = document.getElementById("ageInput").value;
 
-let expireDate = new Date();
-expireDate.setHours(expireDate.getHours()+3);
+      let expireDate = new Date();
+      expireDate.setHours(expireDate.getHours() + 3); // термін дії куки
 
-document.cookie = "user=Sasha; Expires=" + expireDate.toUTCString();
-document.cookie = "age=25; Expires=" + expireDate.toUTCString();
+      document.cookie = "user=" + encodeURIComponent(user) + "; Expires=" + expireDate.toUTCString();
+      document.cookie = "age=" + encodeURIComponent(age) + "; Expires=" + expireDate.toUTCString();
 
+      alert("Куки збережено!");
+    });
 
+    // Зчитування куки (без виводу на сторінку!)
+    let cookies = document.cookie.split("; ");
+    let user = "";
+    let age = "";
 
-let myCookie = "Моя =кука;= складна";
-
-myCookie = encodeURIComponent(myCookie);
-console.log(myCookie);
-
-document.cookie = `complex=${myCookie}`;
-
-
-console.log(decodeURIComponent(myCookie));
-
-
-
-
-let cookies = document.cookie.split("; ");
-let user = "";
-let age = "";
-
-  for (let i = 0; i < cookies.length; i++) {
-    let parts = cookies[i].split("=");
-    if (parts[0] === "user") {
-      user = decodeURIComponent(parts[1]);
+    for (let i = 0; i < cookies.length; i++) {
+      let parts = cookies[i].split("=");
+      if (parts[0] === "user") {
+        user = decodeURIComponent(parts[1]);
+      }
+      if (parts[0] === "age") {
+        age = decodeURIComponent(parts[1]);
+      }
     }
-    if (parts[0] === "age") {
-      age = decodeURIComponent(parts[1]);
-    }
-  }
-
-  if (user && age) {
-    document.write("User: " + user + "<br>Age: " + age);
-  } else {
-    document.write("Кука user або age не знайдена.");
-  }
