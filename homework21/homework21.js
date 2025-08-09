b8644e19cca6013f607d8ee2b4a587a8df6e3e76
Yaroslav66666 <aroslaveremenko6@gmail.com>
2025-08-09 11:06:06 +0300

    homework21

--- homework21/homework21.js
+++ homework21/homework21.js
@@ -1,78 +1,82 @@
 let canva = document.querySelector("#canva-box");
 let ctx = canva.getContext("2d");
 
-
-ctx.fillStyle = "blue";
-ctx.globalAlpha = 0.5;
-
-
 let img = new Image();
- img.src = "./img/images.jpg";
+img.src = "./img/images.jpg";
 
- addEventListener("load", function(e){
- 	console.log("Loaded")
- 	ctx.drawImage(img, 0, 0, canva.width, canva.height);
- 	ctx.globalAlpha = 0.5;
- 	
-  ctx.fillStyle = "blue";
- })
-
-
-let startCords = {x:0, y:0};
-let mouseCords = {x:0, y:0};
-
-ctx.globalAlpha = 0.5;
-ctx.fillStyle = "green"
-
-let rect = canva.getBoundingClientRect();
-console.log(rect);
-
+let startCords = {x: 0, y: 0};
+let mouseCords = {x: 0, y: 0};
 let flag = false;
 
+let colorPicker = document.querySelector("#colorPicker");
+let alphaRange = document.querySelector("#alphaRange");
 
-canva.addEventListener("mousedown", function(e){
-	startCords.x = e.clientX - rect.x;
-	startCords.y = e.clientY - rect.y;
-	flag = true;
-})
-
-
-canva.addEventListener("mousemove", function(e){
-	if (!flag) {return;}
-	e.preventDefault();
-	mouseCords.x = e.clientX - rect.x;
-	mouseCords.y = e.clientY - rect.y;
-	let width = mouseCords.x - startCords.x;
-	let height =  mouseCords.y - startCords.y;
-
-	ctx.clearRect(0, 0, canva.width, canva.height);
-	ctx.fillRect(startCords.x, startCords.y, width, height )
-
-})
+let currentColor = colorPicker.value;
+let currentAlpha = parseFloat(alphaRange.value);
 
 
+function drawBackground() {
+    ctx.drawImage(img, 0, 0, canva.width, canva.height);
+}
 
-canva.addEventListener("mouseup", function(e){
-	flag = false;
 
+img.addEventListener("load", function() {
+    drawBackground();
+});
+
+
+colorPicker.addEventListener("input", function() {
+    currentColor = this.value;
+});
+
+
+alphaRange.addEventListener("input", function() {
+    currentAlpha = parseFloat(this.value);
+});
+
+
+canva.addEventListener("mousedown", function(e) {
+    let rect = canva.getBoundingClientRect();
+    startCords.x = e.clientX - rect.x;
+    startCords.y = e.clientY - rect.y;
+    flag = true;
+});
+
+
+canva.addEventListener("mousemove", function(e) {
+    if (!flag) return;
+    let rect = canva.getBoundingClientRect();
+    mouseCords.x = e.clientX - rect.x;
+    mouseCords.y = e.clientY - rect.y;
+    
+    let width = mouseCords.x - startCords.x;
+    let height = mouseCords.y - startCords.y;
+
+    ctx.clearRect(0, 0, canva.width, canva.height);
+    drawBackground();
+    ctx.globalAlpha = currentAlpha;
+    ctx.fillStyle = currentColor;
+    ctx.fillRect(startCords.x, startCords.y, width, height);
+    ctx.globalAlpha = 1; 
+});
+
+
+canva.addEventListener("mouseup", function() {
+    flag = false;
 });
 
 
 let btn = document.querySelector("#clearAll");
-btn.addEventListener("click", function(e){
-	ctx.clearRect(0, 0, canva.width, canva.height);
+btn.addEventListener("click", function() {
+    ctx.clearRect(0, 0, canva.width, canva.height);
+    drawBackground();
 });
 
 
-btn.addEventListener("mouseenter", function(e){
-	let rand_color = '#'+Math.floor(Math.random()*16777215).toString(16);
-
-	e.target.style.backgroundColor = rand_color;
-
-})
-
-btn.addEventListener("mouseleave", function(e){
-
-	e.target.style.backgroundColor = "#f0f0f0";
-
-})
+btn.addEventListener("mouseenter", function(e) {
+    let rand_color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    e.target.style.backgroundColor = rand_color;
+});
+btn.addEventListener("mouseleave", function(e) {
+    e.target.style.backgroundColor = "#f0f0f0";
+});
